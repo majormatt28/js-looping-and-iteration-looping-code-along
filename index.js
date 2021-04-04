4ed986e07087f10503e8bf0b6e6b0a99e2671916
Matthew <elijah2895@hotmail.com>
2021-04-04 18:47:45 -0400

    commit tree ccc31aa17860a51ba875d2176dad3d5f96d2a602

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, events){
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+    return messages;
+}
+
+function countDown (count){ 
+    while (count > 0) {
+        console.log (count);
+        count -= 1;
+    }
+    console.log (count);
+}
